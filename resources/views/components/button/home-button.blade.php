d33e02bc2e7ae3f91fa8610694dbf0a5330550a7
Densho <sgwykdn@gmail.com>
2025-10-27 19:38:32 +0800

    home

---
+++ resources/views/components/button/home-button.blade.php
@@ -0,0 +1,8 @@
+@props([
+    'hrefLink',
+    'label'
+])
+
+<button {{ $attributes->merge(['class' => 'inline-block px-5 py-1.5 text-[#1b1b18] border-white border py-2 px-5 text-white font-semibold rounded-xl']) }}>
+    <a href="{{$hrefLink ?? ""}}">{{ $slot }}</a>
+</button>
